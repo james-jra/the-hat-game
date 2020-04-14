936e126fbbbd8844bd51e623126f89d4fb4e9fa8
James Andrew <james.andrew@metaswitch.com>
2020-04-14 07:51:19 +0100

    add GET all and PUT for hat picks

--- hat_game/models.py
+++ hat_game/models.py
@@ -6,10 +6,10 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game_id = db.Column(db.String(256), index=True, unique=True)
     created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    hat_picks = db.relationship('HatPick', backref='game', lazy=True)
+    hat_picks = db.relationship("HatPick", backref="game", lazy=True)
 
     def __repr__(self):
-        return '<Game {}>'.format(self.game_id)
+        return "<Game {}>".format(self.game_id)
 
 
 class HatPick(db.Model):
@@ -17,8 +17,18 @@ class HatPick(db.Model):
     name = db.Column(db.String(256))
     picked = db.Column(db.Boolean, default=False)
     submitter = db.Column(db.String(256))
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<HatPick {} in game {}>'.format(self.name, self.game_id)
+        return "<HatPick {} in game {}>".format(self.name, self.game_id)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "picked": self.picked,
+            "submitter": self.submitter,
+            "game_id": self.game_id,
+            "timestamp": self.timestamp,
+        }
--- hat_game/routes.py
+++ hat_game/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import (
     render_template,
     url_for,
@@ -11,9 +12,9 @@ from flask import (
 from hat_game import app, db, id_generator
 from hat_game.forms import GameJoinForm
 from hat_game.models import Game, HatPick
-from sqlalchemy.exc import IntegrityError
 import json
 import random
+from sqlalchemy.exc import IntegrityError
 
 rand = random.SystemRandom()
 
@@ -52,7 +53,7 @@ def create_game():
             db.session.rollback()
 
     if game_id is None:
-        abort(500, "Game creation failed")
+        abort(500, description="Game creation failed")
 
     game_url = url_for("game_page", game_id=game_id)
     resp = make_response("", 201)
@@ -65,7 +66,7 @@ def game_page(game_id):
     # TODO check for user session and redirect if not already playing.
     game = Game.query.filter_by(game_id=game_id).limit(1).first()
     if game is None:
-        abort(404, "Game {game_id} not found")
+        abort(404, description="Game {game_id} not found")
 
     return render_template(
         "game.html",
@@ -75,40 +76,24 @@ def game_page(game_id):
     )
 
 
-@app.route("/game/<string:game_id>/draw", methods=["POST"], strict_slashes=False)
-def draw_name(game_id):
-    names = (
-        HatPick.query.filter_by(picked=False)
-        .join(Game)
-        .filter(Game.game_id == game_id)
-        .all()
-    )
-    if len(names) == 0:
-        return jsonify({"empty": True})
-
-    drawn = rand.choice(names)
-    drawn.picked = True
-    # Write-back the chosen one
-    db.session.commit()
-    return jsonify({"name": drawn.name, "empty": False})
-
-
 @app.route("/game/<string:game_id>/join", methods=["POST", "GET"], strict_slashes=False)
 def join_game(game_id):
     game = Game.query.filter_by(game_id=game_id).limit(1).first()
     if game is None:
-        abort(404, "Game {game_id} not found")
+        abort(404, description="Game {game_id} not found")
 
     # TODO check for user session and redirect if already playing.
     form = GameJoinForm()
     if form.validate_on_submit():
         flash("User {} joined game {}".format(form.username.data, game.game_id))
-        game.hat_picks.extend([
-            HatPick(name=form.names_0.data, submitter=form.username.data),
-            HatPick(name=form.names_1.data, submitter=form.username.data),
-            HatPick(name=form.names_2.data, submitter=form.username.data),
-            HatPick(name=form.names_3.data, submitter=form.username.data),
-        ])
+        game.hat_picks.extend(
+            [
+                HatPick(name=form.names_0.data, submitter=form.username.data),
+                HatPick(name=form.names_1.data, submitter=form.username.data),
+                HatPick(name=form.names_2.data, submitter=form.username.data),
+                HatPick(name=form.names_3.data, submitter=form.username.data),
+            ]
+        )
         db.session.commit()
         return redirect("/game/{}".format(game.game_id))
 
@@ -119,3 +104,71 @@ def join_game(game_id):
         url_root=request.url_root[:-1],
         form=form,
     )
+
+
+@app.route("/game/<string:game_id>/hat_picks", strict_slashes=False)
+def get_game_hat_picks(game_id):
+    hat_picks = HatPick.query.join(Game).filter(Game.game_id == game_id).all()
+
+    return jsonify([hat_pick.as_dict() for hat_pick in hat_picks])
+
+
+@app.route(
+    "/game/<string:game_id>/hat_picks/draw", methods=["POST"], strict_slashes=False
+)
+def draw_name(game_id):
+    hat_picks = (
+        HatPick.query.filter_by(picked=False)
+        .join(Game)
+        .filter(Game.game_id == game_id)
+        .all()
+    )
+    if len(hat_picks) == 0:
+        return jsonify(dict())
+
+    hat_pick = rand.choice(hat_picks)
+    hat_pick.picked = True
+    # Write-back the chosen one
+    db.session.commit()
+    return jsonify({"hat_pick": hat_pick.as_dict()})
+
+
+@app.route(
+    "/game/<string:game_id>/hat_pick/<int:name_id>",
+    methods=["PUT"],
+    strict_slashes=False,
+)
+def update_name(game_id, name_id):
+    hat_pick = HatPick.query.filter_by(id=name_id).one_or_none()
+    if hat_pick is None:
+        abort(404, description="Name {name_id} not found")
+
+    if not request.json:
+        abort(400, description="Request had no JSON body")
+    if "name" in request.json and type(request.json["name"]) is not str:
+        abort(400, description="name invalid")
+    if "submitter" in request.json and type(request.json["submitter"]) is not str:
+        abort(400, description="submitter invalid")
+    if "picked" in request.json and type(request.json["picked"]) is not bool:
+        abort(400, description="picked invalid")
+    if "id" in request.json and type(request.json["id"]) is not int:
+        abort(400, description="id invalid")
+    if "game_id" in request.json and type(request.json["game_id"]) is not int:
+        abort(400, description="game_id invalid")
+
+    hat_pick.name = request.json.get("title", hat_pick.name)
+    hat_pick.submitter = request.json.get("description", hat_pick.submitter)
+    hat_pick.picked = request.json.get("done", hat_pick.picked)
+    hat_pick.timestamp = datetime.utcnow()
+
+    if hat_pick.id != request.json.get("id", hat_pick.id):
+        db.session.rollback()
+        abort(403, "id field is immutable")
+    if hat_pick.game_id != request.json.get("game_id", hat_pick.game_id):
+        db.session.rollback()
+        abort(403, "game_id field is immutable")
+
+    # Write-back the chosen one
+    db.session.commit()
+
+    return jsonify({"hat_pick": hat_pick.as_dict()})
